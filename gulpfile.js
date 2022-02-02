20872295d460642e6172ed9dbeb302282db11163
ArtemCaster <97943938+ArtemCaster@users.noreply.github.com>
2022-02-02 18:07:39 +0300

    added shop page

--- gulpfile.js
+++ gulpfile.js
@@ -29,6 +29,8 @@ function scripts () {
     'node_modules/slick-carousel/slick/slick.js',  //плагины slick
     'node_modules/rateyo/src/jquery.rateyo.js',  //плагины rateyo
     'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',  //плагины fancybox
+    'node_modules/ion-rangeslider/js/ion.rangeSlider.js',  //плагины Range Slider
+    'node_modules/jquery-form-styler/dist/jquery.formstyler.js',  //плагины form stiler
     'app/js/main.js'
   ])
   .pipe(concat('main.min.js'))
